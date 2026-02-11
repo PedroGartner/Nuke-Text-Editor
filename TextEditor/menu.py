2a3f814000851b21f7bb9f096174f3bc0ebe1d86
Pedro Gartner <pedrogartnervelasco@gmail.com>
2026-02-11 16:20:55 +0000

    Update menu.py

--- TextEditor/menu.py
+++ TextEditor/menu.py
@@ -1,3 +1,3 @@
 import TextEditor
-scripts_m = nuke.menu('Nuke').addMenu('PGartner')
+scripts_m = nuke.menu('Nuke').addMenu('Tools')
 scripts_m.addCommand('Text Editor', 'TextEditor.show_texteditor()')
